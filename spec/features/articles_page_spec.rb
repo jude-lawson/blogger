301075d0ddaa0c6f3176a79aae1201957e01b5ab
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-09 14:39:37 -0600

    Adds in create a new item link for articles page

--- spec/features/articles_page_spec.rb
+++ spec/features/articles_page_spec.rb
@@ -1,15 +1,29 @@
 require 'rails_helper'
 
-describe 'user sees all articles' do
-  describe 'they visit /articles' do
-    it 'should display all articles' do
-      article1 = Article.create!(title: 'Title 1', body: 'Body 1')
-      article2 = Article.create!(title: 'Title 2', body: 'Body 2')
+RSpec.describe 'Articles Page' do
 
-      visit '/articles'
+  before(:each) do
+    @article1 = Article.create!(title: 'Title 1', body: 'Body 1')
+    @article2 = Article.create!(title: 'Title 2', body: 'Body 2')
 
-      expect(page).to have_link(article1.title)
-      expect(page).to have_link(article2.title)
+    # Important Page Elements
+    @new_article_link_text = "Create A New Article"
+  end
+  
+  describe 'user sees all articles' do
+    describe 'they visit /articles' do
+      it 'should display all articles' do
+        visit '/articles'
+
+        expect(page).to have_link(@article1.title)
+        expect(page).to have_link(@article2.title)
+      end
+
+      it 'should have a link to create a new article' do
+        visit '/articles'
+
+        expect(page).to have_link(@new_article_link_text)
+      end
     end
   end
 end
